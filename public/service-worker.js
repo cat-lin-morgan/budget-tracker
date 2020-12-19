5db0279977de08698d3a5692dff76d4fd1f5ed20
cat - lin <plummorgan@gmail.com>
2020-12-18 16:36:22 -0800

    service worker fetches from cache and fetches from db when online

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,16 +18,16 @@ const FILES_TO_CACHE = [
     "./icons/icon-512x512.png"
 ];
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
     e.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            console.log('installing cache: ' + CACHE_NAME);
-            return cache.addAll(FILES_TO_CACHE);
-        })
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME);
+            return cache.addAll(FILES_TO_CACHE)
+        }) 
     )
-});
+})
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function (e) {
     e.waitUntil(
         caches.keys().then(function (keyList) {
             let cacheKeepList = keyList.filter(function(key) {
@@ -37,12 +37,28 @@ self.addEventListener('activate', function(e) {
 
             return Promise.all(
                 keyList.map(function(key, i) {
-                    if (cacheKeepList.indexOf(key) === -1) {
-                        console.log('deleting cache: ' + keyList[i]);
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
                         return caches.delete(keyList[i]);
                     }
                 })
             )
+        })
+    )
+})
+
+self.addEventListener('fetch', function(e) {
+    console.log('fetch request: ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function(request) {
+            if (request) {
+                console.log('responding with cache: ' + e.request.url);
+                return request;
+            } else {
+                console.log('file is not cached, fetching: ' + e.request.url);
+                return fetch(e.request);
+            }
+            // return request || fetch(e.request) //would replace the if else's console.log's
         })
     )
 })
